1ac7a620c836adbfceeeff4954913844cee9de13
mike wakerly <opensource@hoho.com>
2019-09-04 23:26:12 -0400

    make the playlist always show the currently-playing item at the top

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -15,9 +15,10 @@ export default function Playlist({ playlist }) {
         return null;
     }
 
+    const currentPosition = playlist.current_position;
     const items = playlist.queue || [];
     const playlistItems = items.map((item, idx) => {
-        const isActive = idx === playlist.current_position;
+        const isActive = idx === currentPosition;
         const className = isActive ? "list-group-item active" : "list-group-item";
         const secondsRemaining = millisToSeconds(playlist.millis_remaining);
         const timeRemaining = isActive && playlist.millis_remaining ?
@@ -26,10 +27,15 @@ export default function Playlist({ playlist }) {
         return (<ul key={idx} className={className}>{itemName}</ul>);
     });
 
+    const sortedItems = [
+        ...playlistItems.slice(currentPosition, playlistItems.length),
+        ...playlistItems.slice(0, currentPosition),
+    ];
+
     return (
         <div className="card card-body bg-light" style={STYLE}>
             <ul className="list-group">
-                {playlistItems}
+                {sortedItems}
             </ul>
         </div>
     );
