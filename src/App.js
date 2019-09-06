9a803c4484dd3969a144cff3ceb38109965f724d
mike wakerly <opensource@hoho.com>
2019-09-05 20:01:48 -0400

    import bootswatch; use the `cyborg` dark theme!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Navbar, Nav, Form, Button } from 'react-bootstrap';
 
 import DanceFloorController from './components/DanceFloorController';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootswatch/dist/cyborg/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
+    <>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">DDF-GW1</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+        </Nav>
+        <Form inline>
+          <Button variant="outline-success">Reload</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
     <Container>
       <DanceFloorController />
     </Container>
+    </>
   );
 }
 
